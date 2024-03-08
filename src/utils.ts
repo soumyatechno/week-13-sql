96acb4105e8944556162159915a3816ccc31850a
soumya <soumya.chakraborty@hoichoi.tv>
2024-03-08 13:35:12 +0530

    setup

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import { Client } from 'pg';
 
 export async function getClient() {
-    const client = new Client("postgres://wzsxsnxg:LHZ9Cv4QoZ1zctxapkOq2ch672-o9UQe@trumpet.db.elephantsql.com/wzsxsnxg");
+    const client = new Client("postgres://bwxilptl:MKx9OpS95ytMZjXbluGeEJ2cobqlOJIY@satao.db.elephantsql.com/bwxilptl");
     await client.connect();
     return client;
 }
